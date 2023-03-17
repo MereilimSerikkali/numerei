d6580b9ef03fcee10d594598ba8cc8d56cacbd51
Mereilim <121162623+MereionMission@users.noreply.github.com>
2023-03-17 13:23:55 -0700

    Delete java.js

--- java.js
+++
@@ -1,8 +0,0 @@
-let getStartedBtn = document.getElementById("get-started-btn");
-if (localStorage.getItem("getStartedClicked")) {
-  getStartedBtn.style.display = "none";
-}
-getStartedBtn.addEventListener("click", function() {
-  localStorage.setItem("getStartedClicked", true);
-  getStartedBtn.style.display = "none";
-});
